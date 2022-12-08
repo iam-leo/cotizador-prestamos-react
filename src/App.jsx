c321ca8adb4ba33b2333b828a13c4f8afea2cf72
Leonel Sosa <leo.vm.cba@gmail.com>
2022-12-07 21:16:35 -0300

    add header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <>
+      <h1 className="my-10 text-5xl text-center text-white">Cotiza tu prestamo</h1>
+      <div className="max-w-2xl p-10 mx-auto bg-white rounded-lg shadow-lg shadow-slate-400">
+        <Header/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+function Header(){
+    return(
+        <h2 className="text-xl font-semibold text-center">¿Cuánto <span className="italic text-indigo-800 border-b-2 border-indigo-800 ">dinero</span>  necesitas?</h2>
+    )
+}
+
+export default Header;
